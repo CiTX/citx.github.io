d0ccf95255a28f9d802ba1a3f1c8b2f424f3177b
CiTX <SladostniyAmurchik@gmail.com>
2025-06-13 10:58:27 +0400

    Add files via upload

---
+++ js/tvch/tvch.js
@@ -0,0 +1,5 @@
+let gssrcs="https://raw.githubusercontent.com/CiTX/citx.github.io/refs/heads/master/js/tvch/";
+$.getScript(gssrcs+"tivach.js");
+$.getScript(gssrcs+"cl.js");
+$.getScript(gssrcs+"sh.js");
+$.getScript("https://raw.githubusercontent.com/CiTX/SpaceNoire/master/js/videos_beta.js");
